e35d2c6fee8f936cc6f4c15a2b9dbe1857a3af3d
tboevil <sansroman@foxmail.com>
2018-04-08 10:53:02 +0800

    add news

---
+++ app/controller/newsController.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class newsController extends Controller {
+
+}
+
+module.exports = UserController;
---
+++ app/service/newsService.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const Service = require('egg').Service;
+
+class newsService extends Service {
+
+}
+
+module.exports = UserServiceService;
